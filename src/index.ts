1313c3491bf1812a9944c743afcdb7cc245933fb
Ana Zimerer <ana.zimerer@gmail.com>
2020-08-19 14:15:49 -0300

    Services e Datas

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+import { IdGenerator } from "./services/IdGenerator";
+import { UserDatabase } from "./data/UserDatabase";
+import { Authenticator } from "./services/Authenticator";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+//app.post("/signup",Signup)
+//app.post("/login", Login)
+//app.get("/user/profile", GetUserProfile)
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
