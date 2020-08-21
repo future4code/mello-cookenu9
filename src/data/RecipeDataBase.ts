d45e91478cd8b31f58551c4b919416096eed4d92
Ana Zimerer <ana.zimerer@gmail.com>
2020-08-21 16:38:20 -0300

    Endpoint: Feed de receitas

--- src/data/RecipeDataBase.ts
+++ src/data/RecipeDataBase.ts
@@ -1,4 +1,5 @@
 import { BaseDatabase } from "./BaseDatabase";
+import { UserDatabase } from "./UserDatabase";
 
 interface Recipe {
   id: string;
@@ -8,25 +9,35 @@ interface Recipe {
 }
 
 export default class RecipeDataBase extends BaseDatabase {
-  private static TABLE_NAME = "cookenu_recipe";
+  	private static TABLE_NAME = "cookenu_recipe";
 
-  public async createRecipe(recipe: Recipe): Promise<void> {
-    await this.getConnection()
-      .insert({
-        id: recipe.id,
-        title: recipe.title,
-        description: recipe.description,
-        created_at: recipe.created_at,
-      })
-      .into(RecipeDataBase.TABLE_NAME);
-  }
+  	public async createRecipe(recipe: Recipe): Promise<void> {
+  	  await this.getConnection()
+  	    .insert({
+  	      id: recipe.id,
+  	      title: recipe.title,
+  	      description: recipe.description,
+  	      created_at: recipe.created_at,
+  	    })
+  	    .into(RecipeDataBase.TABLE_NAME);
+  	}
 
-  public async getRecipeById(id: string): Promise<any> {
-    const result = await this.getConnection()
-      .select("*")
-      .from(RecipeDataBase.TABLE_NAME)
-      .where({ id });
+  	public async getRecipeById(id: string): Promise<any> {
+  	  const result = await this.getConnection()
+  	    .select("*")
+  	    .from(RecipeDataBase.TABLE_NAME)
+  	    .where({ id });
 
-    return result[0];
-  }
+  	  return result[0];
+  	}
+
+  	public async getRecipeFeed():Promise<any>{
+		const result = await this.getConnection().raw(`
+			SELECT * FROM cookenu_recipe
+			JOIN cookenu_user
+			ON  cookenu_recipe.user_id = cookenu_user.id;
+			`)	
+		console.log(result[0])
+  		return result[0];
+	}
 }
--- src/endpoints/unfollow.ts
+++ src/endpoints/unfollow.ts
@@ -1,27 +1,23 @@
-// import { Request, Response } from "express";
-// import { UserDatabase } from "../data/UserDatabase";
-// import { Authenticator } from "../services/Authenticator";
-// import { BaseDatabase } from "../data/BaseDatabase";
+ import { Request, Response } from "express";
+ import { UserDatabase } from "../data/UserDatabase";
+ import { Authenticator } from "../services/Authenticator";
+ import { BaseDatabase } from "../data/BaseDatabase";
 
-// export default async function UnFollow(req: Request, res: Response) {
-//   try {
-//     const token = req.headers.authorization as string;
-//     const autenticator = new Authenticator();
-
-//     const autenticatorData = autenticator.getData(token);
-
-//     const unfollowedId = req.body.unfollowedId as string;
-
-//     await new UserDatabase().unfollow(unfollowedId);
-
-//     res.status(200).send({
-//       message: "deixou de seguir",
-//     });
-//   } catch (err) {
-//     res.status(400).send({
-//       message: err.message,
-//     });
-//   } finally {
-//     await BaseDatabase.destroyConnection();
-//   }
-// }
+ export default async function UnFollow(req: Request, res: Response) {
+    try {
+        const token = req.headers.authorization as string;
+        const autenticator = new Authenticator();
+        const autenticatorData = autenticator.getData(token);
+        const unfollowedId = req.body.unfollowedId as string;
+        await new UserDatabase().unfollow(unfollowedId);
+        res.status(200).send({
+          message: "deixou de seguir",
+        });
+    }catch (err) {
+        res.status(400).send({
+          message: err.message,
+        });
+    }finally {
+        await BaseDatabase.destroyConnection();
+    }
+ }
