6ab83ceda96e0481e5155981f79e71473d5b3f5f
Kaueny de Oliveira Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2020-08-21 13:40:21 -0300

    Endpoint de Seguir usuário

--- src/endpoints/getRecipe.ts
+++ src/endpoints/getRecipe.ts
@@ -1,33 +1,32 @@
 import { Request, Response } from "express";
-import { RecipeDataBase } from "../data/RecipeDatabase";
-import moment from 'moment'
+import moment from "moment";
+import RecipeDataBase from "../data/RecipeDataBase";
 
-export default async function GetRecipeById (req: Request, res: Response){
-    try {
-        const token = req.headers.authorization as string;
-        const id= req.params.id as string; 
+export default async function GetRecipeById(req: Request, res: Response) {
+  try {
+    const token = req.headers.authorization as string;
+    const id = req.params.id as string;
 
-        if (!token || !id ){
-            throw new Error ("Token ou id inválidos")
-        }
-        
-        const recipeDb = new RecipeDataBase();
-        const recipe = await recipeDb.getRecipeById(id);
-        
-        const recipeCreatedAt = recipe.created_at
-        res.status(200).send({
-          id: recipe.id,
-          title: recipe.title,
-          description: recipe.description,
-          created_at: moment(recipeCreatedAt,'DD/MM/YYYY')
-        });
+    if (!token || !id) {
+      throw new Error("Token ou id inválidos");
+    }
 
-    }   catch (err) {
-        res.status(400).send({
-          message: err.message,
-        });
-    }   
-    //  finally{
-          //await BaseDatabase.destroyConnection()
-    //  }   
-  };    
+    const recipeDb = new RecipeDataBase();
+    const recipe = await recipeDb.getRecipeById(id);
+
+    const recipeCreatedAt = recipe.created_at;
+    res.status(200).send({
+      id: recipe.id,
+      title: recipe.title,
+      description: recipe.description,
+      created_at: moment(recipeCreatedAt, "DD/MM/YYYY"),
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+  //  finally{
+  //await BaseDatabase.destroyConnection()
+  //  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { AddressInfo } from "net";
 
-import Signup from './endpoints/signup';
-import Login from './endpoints/login';
-import GetUserProfile from './endpoints/getUserProfile'
-import CreateRecipe from './endpoints/createRecipe'
+import Signup from "./endpoints/signup";
+import Login from "./endpoints/login";
+import GetUserProfile from "./endpoints/getUserProfile";
+import CreateRecipe from "./endpoints/createRecipe";
 import GetRecipeById from "./endpoints/getRecipe";
-import GetUserById from "./endpoints/GetUserById";
-
+import GetUserById from "./endpoints/getUserById";
+import Follow from "./endpoints/follow";
+//import UnFollow from "./endpoints/unfollow";
 
 dotenv.config();
 const app = express();
@@ -17,12 +18,15 @@ app.use(express.json());
 app.post("/user/signup", Signup);
 app.post("/user/login", Login);
 app.get("/user/profile", GetUserProfile);
+app.post("/user/follow", Follow);
+//app.post("/user/unfollow", UnFollow);
+
 app.get("/user/:id", GetUserById);
 
-app.post("/recipe", CreateRecipe)
-app.get("/recipe/:id", GetRecipeById)
+app.post("/recipe", CreateRecipe);
+app.get("/recipe/:id", GetRecipeById);
 
-const server = app.listen(process.env.PORT || 3003, () => {
+const server = app.listen(process.env.PORT || 3306, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Server is running in http://localhost:${address.port}`);
